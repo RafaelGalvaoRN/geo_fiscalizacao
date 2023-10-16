0166122edbf40dc9fd6a6339a33b3d37d5157f9a
RafaelGalvaoRN <rafael.galvao@mprn.mp.br>
2023-10-16 17:47:34 -0300

    documentacao

---
+++ pages/09_Documentação.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.subheader("Atualizações 🚀")  # Adicionando emoji para dar destaque
+st.markdown("---")  # Linha horizontal para separar o conteúdo
+
+# Estilizando a data com negrito
+st.markdown("**16/10/2023**")
+# Adicionando bullet point manualmente para destaque
+st.markdown(
+    "- 1º Versão do sistema, que se encontra em fase de ajustes.")
+# Você pode continuar adicionando outras atualizações da mesma forma
+
+st.markdown("---")  # Outra linha horizontal
